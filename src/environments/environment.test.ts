6eeb6524ce92d28ab9953d56a365e00625c4fd16
Jayvee Ramos <Jayvee@ag-investment.net>
2019-12-03 16:12:47 +0800

    -added two api configurations for cashier and send payment

--- src/environments/environment.test.ts
+++ src/environments/environment.test.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  api: 'http://telfapay-cashier.fyjutnxsbd.ap-east-1.elasticbeanstalk.com',
-  cashier_api: 'http://telfa-api-test.ap-northeast-1.elasticbeanstalk.com',
+  api: 'http://telfa-api-test.ap-northeast-1.elasticbeanstalk.com',
+  cashier_api: 'http://telfapay-cashier.fyjutnxsbd.ap-east-1.elasticbeanstalk.com',
   production: true
 };
