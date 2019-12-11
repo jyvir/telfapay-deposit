91b5cb7c21274240d563e062b340ed6006cce901
Jayvee Ramos <Jayvee@ag-investment.net>
2019-12-11 13:18:53 +0800

    changed prod environment.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  api: 'http://localhost:9292',
-  cashier_api: 'http://localhost:9292',
+  api: 'https://cashier.marginpayment.com',
+  cashier_api: 'https://cashier.marginpayment.com',
   production: true
 };
