511af81dbbb514db82dde249f514048eac2b060b
Jayvee Ramos <Jayvee@ag-investment.net>
2019-12-18 12:58:53 +0800

    -removed slash

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  api: 'https://cashierapi.luckycashment.com:9190/',
-  cashier_api: 'https://cashierapi.luckycashment.com:9190/',
+  api: 'https://cashierapi.luckycashment.com:9190',
+  cashier_api: 'https://cashierapi.luckycashment.com:9190',
   production: true
 };
