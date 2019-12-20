e64f12ff7ca3fb3a93017cdcbb8140793d75aeed
jmyuhan <55520700+jmyuhan@users.noreply.github.com>
2019-12-20 16:58:36 +0800

    Update environment.prod.ts
    test

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  api: 'https://cashierapi.luckycashment.com:9190',
+  api: 'https://cashierapi.luckycashment.com:9190s',
   cashier_api: 'https://cashierapi.luckycashment.com:9190',
   production: true
 };
