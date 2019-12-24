7be53d68324c746d7cf063e8d26ea7e83edc549b
telfapay <telfa@cmtspace.com>
2019-12-24 16:08:32 +0800

    api domian from https://dsd.com change to https://cashierapi.test.com

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  api: 'https://dsd.com',
-  cashier_api: 'https://dsd.com',
+  api: 'https://cashierapi.test.com',
+  cashier_api: 'https://cashierapi.test.com',
   production: true
 };
