859e507866a1160d3aaecc54f10c9d2bdac1671f
Jayvee Ramos <Jayvee@ag-investment.net>
2019-12-28 11:31:27 +0800

    -hide loading after browser open

--- src/app/modules/deposit/cashier-tab/cashier-tab.component.ts
+++ src/app/modules/deposit/cashier-tab/cashier-tab.component.ts
@@ -156,6 +156,7 @@ export class CashierTabComponent implements OnInit, AfterViewInit {
       if (payload.channel !== 'BANK' && payload.channel !== 'OFFLINE_BANK' && this.cookie.get('cashier_script') === 'true') {
         const token = this.cookie.get('token');
         ext.call(`${environment.cashier_api}/cashier/deposit-get?${req}&token=${token}`);
+        this.loading = false;
         return true;
       }
       this.commonService.sendPayment('', req).pipe(
